cfec75dfe46553098f2a9ddafa7ded82b41b41e9
Jorge-Prilux <j.millan@grupoprilux.com>
2023-12-18 18:40:36 +0100

    Progress in Testimonials and About sections

--- src/components/Testimonials/Testimonials.js
+++ src/components/Testimonials/Testimonials.js
@@ -1,42 +1,51 @@
 import Testimony from "./Testimony"
+import CarlosRamirez from "../../assets/Carlos Ramirez.jpg"
+import LucaMoretti from "../../assets/Luca Moretti.jpg"
+import OliviaHarper from "../../assets/Olivia Harper.jpg"
+import SophiaRodriguez from "../../assets/Sophia Rodriguez.jpg"
+import "./Testimony.css"
 
 const Testimonials = () => {
     const opinions = [
         {
-            rating: 5,
-            photo: "",
-            name: "",
-            opinion: ""
+            rating: 4,
+            photo: SophiaRodriguez,
+            name: "Sophia Rodriguez",
+            opinion: "The Greek Salad at Little Lemon is a flavor explosion! Fresh, vibrant, and a true taste of the Mediterranean—this dish is a must-try."
 
         },
         {
             rating: 5,
-            photo: "",
-            name: "",
-            opinion: ""
+            photo: LucaMoretti,
+            name: "Luca Moretti",
+            opinion: "Mario and Adrian's passion for cooking shines through every dish at Little Lemon, making it my go-to spot for an unforgettable Mediterranean culinary experience in Chicago."
 
         },
         {
             rating: 5,
-            photo: "",
-            name: "",
-            opinion: ""
+            photo: CarlosRamirez,
+            name: "Carlos Ramirez",
+            opinion: "Little Lemon is where innovation meets tradition, and the Lemon Cake is proof. A sweet symphony that transports you to the sun-soaked shores with every heavenly bite."
 
         },
         {
             rating: 5,
-            photo: "",
-            name: "",
-            opinion: ""
+            photo: OliviaHarper,
+            name: "Olivia Harper",
+            opinion: "I've never experienced Mediterranean cuisine quite like this. Little Lemon's modern twist on classics is a culinary journey that keeps me coming back for more. Bravo!"
 
         },
     ]
-    return <>
-    <h1>Testimonials</h1>
-    {opinions.map((item) => {
-        return <Testimony></Testimony>
-    })}
-    </>
+
+    return (<div className="testimonySection">
+        <h1>Testimonials</h1>
+        <div className="testimonials">
+            {opinions.map((item, index) => (
+            <Testimony key={index} {...item} />
+            ))}
+        </div>
+        </div>
+    );
 }
 
 export default Testimonials
--- src/components/Testimonials/Testimony.js
+++ src/components/Testimonials/Testimony.js
@@ -1,5 +1,23 @@
-const Testimony = () => {
-    return <></>
-}
+import "./Testimony.css"
 
-export default Testimony
+const Testimony = ({ photo, name, rating, opinion }) => {
+
+    // Function to generate stars based on the rating
+  const renderStars = () => {
+    const stars = Array.from({ length: rating }, (_, index) => (
+      <span key={index}>&#9733;</span>
+    ));
+    return stars;
+  };
+
+    return (
+      <div className="testimony">
+        <img src={photo} alt={name} />
+        <h3>{name}</h3>
+        <div className="rating">{renderStars()}</div>
+        <p>{opinion}</p>
+      </div>
+    );
+  };
+
+  export default Testimony;
