8cb383d347a89674792e97241d61229cbec70f02
Jorge-Prilux <j.millan@grupoprilux.com>
2024-01-23 11:42:20 +0100

    Add few usability fixes

--- src/components/Booking/BookingPage.js
+++ src/components/Booking/BookingPage.js
@@ -2,9 +2,12 @@ import BookingForm from "./BookingForm"
 import './Booking.css'
 
 const BookingPage = (props) => {
-    return <div className="booking">
-    <BookingForm props={props} />
-    </div>
+    return<>
+        <h1 style={{background: "#495e49", margin:"0px", padding:"20px", color:"#EDEFEE"}}>Reservations</h1>
+        <div className="booking">
+            <BookingForm props={props} />
+        </div>
+        </>
 }
 
 export default BookingPage
